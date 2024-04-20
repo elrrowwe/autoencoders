be6535d2a2e48cd73b78fa4bdb601abb28d663b2
elrrowwe <248607@edu.p.lodz.pl>
2024-04-20 20:31:01 +0200

    vae loss

--- train_scripts/train_vae.py
+++ train_scripts/train_vae.py
@@ -4,3 +4,28 @@ import torchvision
 import torchvision.datasets as datasets
 import matplotlib.pyplot as plt
 
+from models.variational_ae import VAE
+
+
+"""
+The training file for the Variational Autoencoder model. 
+Model training and inference are run on CPU. 
+"""
+
+TRAIN_ITERS = 1000
+CHECKPOINT_ITERS = 100
+
+transform = torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0,), (1,))
+                             ])
+
+# loading the MNIST digits dataset; simultaneously separating it into train/test portions
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+# initializing the VAE model
+vae = VAE(28, 128, 512)
+
+mse_loss = nn.MSELoss()
---
+++ utils/kldiv.py
@@ -0,0 +1,24 @@
+import torch.nn.functional as F
+import torch
+
+
+"""
+The loss for the Variational Autoencoder model.
+Apart from the binary cross entropy term, also includes the KL divergence term. 
+"""
+
+def kldiv_loss(x_pred, x_true, mean, log_var):
+    """
+    Computes the loss for the Variational Autoencoder model.
+
+    :param x_pred: the reconstructed input to the VAE model;
+    :param x_true: the input, untouched;
+    :param mean: the mean produced by the encoder;
+    :param log_var: the log variance produced by the encoder;
+    :return: the loss.
+    """
+    bce_loss = F.binary_cross_entropy(x_pred, x_true)
+    kldiv = - (0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp()))
+    loss = bce_loss + kldiv
+
+    return loss
